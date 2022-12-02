f720f387dad04efbd1dda2ce148f862e0c00d52b
Anit <anitkumar.rk@gmail.com>
2022-12-02 21:53:35 +0000

    initial commit

---
+++ Node.cs
@@ -0,0 +1,48 @@
+﻿public class Node
+{
+    public int data;
+    public Node next;
+}
+public class LinkedList
+{
+    private Node head;
+
+    public void AddFirst(int nodeValue)
+    {
+        Node toAdd = new Node();
+        toAdd.data = nodeValue;
+        toAdd.next = head;
+        head = toAdd;
+    }
+
+    public void AddLast(int nodeValue)
+    {
+        if (head == null)
+        {
+            head = new Node();
+            head.data = nodeValue;
+            head.next = null;
+        }
+        else
+        {
+            Node toAdd = new Node();
+            toAdd.data = nodeValue;
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = toAdd;
+
+        }
+    }
+    public void PrintNodes()
+    {
+        Node current = head;
+        while (current != null)
+        {
+            Console.Write(current.data + "->");
+            current = current.next;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Add First:");
+LinkedList addedAtStart = new LinkedList();
+
+addedAtStart.AddFirst(1);
+addedAtStart.AddFirst(2);
+addedAtStart.AddFirst(3);
+addedAtStart.AddFirst(4);
+
+addedAtStart.PrintNodes();
+
+Console.WriteLine();
+Console.WriteLine("Add Last:");
+LinkedList addedAtEnd = new LinkedList();
+
+addedAtEnd.AddLast(1);
+addedAtEnd.AddLast(2);
+addedAtEnd.AddLast(3);
+addedAtEnd.AddLast(4);
+
+addedAtEnd.PrintNodes();
+
+Console.WriteLine();
+Console.ReadLine();
